5ef0cfb1abcc62c69056fe0e84aa06ae59544b60
Paulo Neto <31972103+pauloofrancisco07@users.noreply.github.com>
2021-01-26 15:28:37 -0300

    Commit Criando Projeto e tabuleiro

---
+++ TechTeste_BatalhaNaval/BatalhaNaval.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechTeste_BatalhaNaval
+{
+    public class BatalhaNaval
+    {
+        public String[,] m_matrizTabuleiro = new String[11, 11];
+
+        public BatalhaNaval()
+        {
+            m_matrizTabuleiro = preencheDefaultTabuleiro(m_matrizTabuleiro);
+        }
+
+
+
+        private String[,] preencheDefaultTabuleiro(String[,] p_matrizTabuleiro)
+        {
+            String[] v_ColunaPadrao = { " A ", " B ", " C ", " D ", " E ", " F ", " G ", " H ", " I ", " J " };
+            String[] v_LinhaPadrao = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", " 10 " };
+
+            for (int j = 0; j<=10; j++)
+            {              
+                for (int i = 0; i <= 10; i++)
+                {
+                    if (i == 0 && j != 0)
+                    {
+                        p_matrizTabuleiro[j, i] = v_ColunaPadrao[j-1];
+                    }
+                    else if(i != 0 && j == 0)
+                    {
+                        p_matrizTabuleiro[j, i] = v_LinhaPadrao[i-1];
+                    }
+                    else if(i != 0 && j != 0)
+                    {
+                        p_matrizTabuleiro[j, i] = " - ";
+                    }
+                    else
+                    {
+                        p_matrizTabuleiro[j, i] = "   ";
+                    }
+                }
+            }
+
+            return p_matrizTabuleiro;
+        }
+
+    }
+}
---
+++ TechTeste_BatalhaNaval/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TechTeste_BatalhaNaval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BatalhaNaval v_Batalha = new BatalhaNaval();
+
+            for (int j = 0; j <= 10; j++)
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.Write(v_Batalha.m_matrizTabuleiro[j, i] + " ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
