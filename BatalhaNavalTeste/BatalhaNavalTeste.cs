50b870b1a51977e8c947ca41d9e2728a96a2aef1
Paulo Neto <31972103+pauloofrancisco07@users.noreply.github.com>
2021-01-26 15:32:12 -0300

    Arquivo de teste Batalha Naval

---
+++ BatalhaNavalTeste/BatalhaNavalTeste.cs
@@ -0,0 +1,27 @@
+using System;
+using NUnit.Framework;
+using TechTeste_BatalhaNaval;
+
+namespace BatalhaNavalTeste
+{
+    public class BatalhaNavalTeste
+    {      
+
+        [Test]
+        public void ConstruindoTabuleiro()
+        {
+            // Construindo um tabuleiro
+            BatalhaNaval v_BatalhaNaval = new BatalhaNaval();
+        }
+
+        public void adicionandoNavios()
+        {
+            // Construindo um tabuleiro
+            BatalhaNaval v_BatalhaNaval = new BatalhaNaval();
+            
+
+
+        }
+
+    }
+}
